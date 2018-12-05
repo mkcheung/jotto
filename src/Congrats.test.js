8f2d40df201930728eb65fcbb27a1cb36677abbf
mcheung <Mars@admins-MacBook-Pro-6.local>
2018-12-05 13:23:38 -0800

    move enzyme configation to setuptests.js

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import checkPropTypes from 'check-prop-types';
 
 import Congrats from './components/Congrats';
 import { checkProp, findByTestAttr } from '../test/testUtils';
 
-Enzyme.configure({
-	adapter: new EnzymeAdapter()
-});
 
 const defaultProps = {
 	success: false
