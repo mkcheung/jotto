10e9a884eff6cf8329276f293b0345d4c8cb8e32
mcheung <Mars@admins-MacBook-Pro-6.local>
2018-12-05 14:45:21 -0800

    add bootstrap and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Congrats from './components/Congrats';
+import GuessedWords from './components/GuessedWords';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={false}/>
+        <GuessedWords guessedWords={[]}/>
       </div>
     );
   }
