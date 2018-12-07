eb2abd7d5be5a9a8e7e372a166390d6d97a8fdb0
mcheung <Mars@admins-MacBook-Pro-6.local>
2018-12-06 16:37:15 -0800

    add initial guessed word test apparatus

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,9 @@ import { combineReducers } from 'redux';
 
 import success from './successReducer';
 
+import guessedWords from './guessedWordsReducer';
+
 export default combineReducers({
 	success,
+	guessedWords
 });
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,11 +1,12 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 export const storeFactory = ( initialState ) => { 
-
-	return createStore(rootReducer, initialState);
+	const createStoreWithMiddleWare = applyMiddleware(...middlewares)(createStore);
+	return createStoreWithMiddleWare(rootReducer, initialState);
 }
 /**
  * Return ShallowWrapper containing node(s) with the given data-test value.
